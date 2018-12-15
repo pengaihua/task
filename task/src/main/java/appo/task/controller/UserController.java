77876e97669945d415774bca526df780756daf23
pengaihua <pengaihua@163.com>
2018-12-15 18:17:59 +0800

    增加邮件和任务

--- task/src/main/java/appo/task/controller/UserController.java
+++ task/src/main/java/appo/task/controller/UserController.java
@@ -1,9 +1,14 @@
 package appo.task.controller;
 
-import java.util.List; 
+import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PathVariable; 
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +21,31 @@ import appo.task.mapper.UserMapper;
 
  @RestController 
  public class UserController { 
- 	 
+	 @Autowired
+	 private JavaMailSender mailSender;
+	 
  	@Autowired 
  	private UserMapper userMapper; 
  	 
  	@RequestMapping("/getUsers") 
  	public List<UserEntity> getUsers() { 
  		List<UserEntity> users=userMapper.getAll(); 
+ 		
+ 		try {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("ahpeng@appotronics.cn");
+        message.setTo("ahpeng@appotronics.cn");
+        message.setSubject("主题：简单邮件");
+        message.setText("<html><h1>这是一个测试邮件</h1><table><tr><td>1234</td><td>23432</td></tr></table></html>");
+
+        mailSender.send(message); }
+ 		catch(Exception ex)
+ 		{
+ 			ex.printStackTrace();
+ 		}
+ 		sendHtml();
+ 		System.out.println("send mail");
+ 		
  		return users; 
  	} 
  	 
@@ -47,5 +70,25 @@ import appo.task.mapper.UserMapper;
      	userMapper.delete(id); 
      } 
       
+     private void  sendHtml() {
+    	    MimeMessage message = null;
+    	    try {
+    	        message = mailSender.createMimeMessage();
+    	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+    	        helper.setFrom("ahpeng@appotronics.cn");
+    	        helper.setTo("ahpeng@appotronics.cn");
+    	        helper.setSubject("标题：发送Html内容");
+
+    	        StringBuffer sb = new StringBuffer();
+    	        sb.append("<h1>大标题-h1</h1>")
+    	          .append("<p style='color:#F00'>红色字</p>")
+    	          .append("<p style='text-align:right'>右对齐</p>");
+    	        helper.setText(sb.toString(), true);
+    	    } catch (MessagingException e) {
+    	        e.printStackTrace();
+    	    }
+
+    	    mailSender.send(message);
+     }
       
  } 
